0df33ad6c2f0255dd74d83416b606301da740e28
Conor Ginty <conorginty@gmail.com>
2021-12-28 21:18:16 +0000

    Lec 39: Default Profile

--- src/main/java/guruspringframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guruspringframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package guruspringframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"}) // We can have multiple active beans, and by making one of the Profiles "default", this will be what's injected in when there are no Active Profiles
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
