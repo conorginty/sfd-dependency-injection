c5ab886f0fa3942910253831e4f38ba994d57d95
Conor Ginty <conorginty@gmail.com>
2021-12-29 17:36:56 +0000

    Lec 41: Spring Bean Life Cycle Demo

---
+++ src/main/java/guruspringframework/sfgdi/services/CustomBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package guruspringframework.sfgdi.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+        System.out.println("Currently inside: CustomBeanPostProcessor.postProcessBEFOREInitialization() method - beanName = " + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+        System.out.println("Currently inside: CustomBeanPostProcessor.postProcessAFTERInitialization() method - beanName = " + beanName);
+        return bean;
+    }
+}
