740d4b31d9db651f5e8a725f43509d9fc75542e2
Conor Ginty <conorginty@gmail.com>
2022-01-11 21:16:04 +0000

    Lec 88: Using Properties Source

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -2,16 +2,30 @@ package guru.springframework.sfgdi.config;
 
 import com.springframework.greetings.I18nEnglishGreetingService;
 import com.springframework.greetings.I18nSpanishGreetingService;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
+@PropertySource("classpath:datasource.properties") // Loads the Properties from the Target File into the Context to then be injected in at Runtime
 @ImportResource("classpath:sfgdi-config.xml") // Notifies Spring of the XML config file
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
+                                  @Value("${guru.password}") String password,
+                                  @Value("${guru.jdbcurl}") String jdbcUrl) {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcUrl(jdbcUrl);
+        return fakeDataSource;
+    }
 
     @Bean
     EnglishGreetingRepository englishGreetingRepository() {
